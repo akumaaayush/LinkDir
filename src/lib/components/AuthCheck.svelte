9e6d34d45c5c979729a3d972177997e36ebf4d14
Aayush Shah <58813006+akumaaayush@users.noreply.github.com>
2023-08-31 20:10:59 +0545

    add: google auth

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { auth } from "$lib/firebase";
+  import { userStore } from "sveltefire";
+
+  const user = userStore(auth);
+</script>
+
+{#if $user}
+  <slot />
+{:else}
+  <p class="text-error">You must be logged in to view this page!</p>
+  <a class="btn btn-primary" href="/login"> Sign in</a>
+{/if}
