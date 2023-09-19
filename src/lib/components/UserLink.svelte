5f5290b7136db9e752fb83e864f6d6ee1bf52305
Aayush Shah <58813006+akumaaayush@users.noreply.github.com>
2023-09-19 18:20:48 +0545

    edit: user's links component

--- src/lib/components/UserLink.svelte
+++ src/lib/components/UserLink.svelte
@@ -1,13 +1,20 @@
 <script>
-  export let icon = "default";
-  export let url = "http://defullturl.com";
+  export let icon = "custom";
+  export let url = "https://www.example.com";
   export let title = "a default title";
 </script>
 
 <a
   href={url}
-  class="stack w-full text-center bg-base-300 flex justify-center items-center p-4 no-underline rounded-lg not-prose"
+  class="stack w-full text-center bg-base-300 flex justify-center items-center p-4 no-underline rounded-lg not-prose
+  "
 >
-  <img src={`/${icon}.png`} alt={icon} height="32" width="32" class="w-8" />
+  <img
+    src={`/${icon}.png`}
+    alt={icon}
+    height="32"
+    width="32"
+    class="w-8 mx-3"
+  />
   <span class="font-bold text-lg text-white">{title}</span>
 </a>
