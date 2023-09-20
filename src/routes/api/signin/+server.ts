e0d5812b6d3a2cade7f9ae5aa953396f77b8147e
Aayush Shah <58813006+akumaaayush@users.noreply.github.com>
2023-09-20 17:42:13 +0545

    add: cookie auth for server-side

---
+++ src/routes/api/signin/+server.ts
@@ -0,0 +1,31 @@
+import { adminAuth } from "$lib/server/admin";
+import { error, json } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+  const { idToken } = await request.json();
+  console.log(idToken);
+  const expiresIn = 60 * 60 * 24 * 5 * 1000; //for 5 days
+  const decodeIdToken = await adminAuth.verifyIdToken(idToken);
+
+  //check if user authenticated recently
+  if (new Date().getTime() / 1000 - decodeIdToken.auth_time < 5 * 60) {
+    const cookie = await adminAuth.createSessionCookie(idToken, { expiresIn });
+
+    //firebase "__session" convention
+    cookies.set("__session", cookie, {
+      maxAge: expiresIn,
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    });
+    return json({ status: "signedIn" });
+  } else {
+    throw error(403, "Recent sign is required!");
+  }
+};
+
+export const DELETE: RequestHandler = async ({ cookies }) => {
+  cookies.delete("__session", { path: "/" });
+  return json({ status: "signedOut" });
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,23 +5,33 @@
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
-    const loginUser = await signInWithPopup(auth, provider);
-    // console.log(loginUser);
+    const credential = await signInWithPopup(auth, provider);
+
+    const idToken = await credential.user.getIdToken();
+    const res = await fetch("api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //csrf handled by sveltekit
+      },
+      body: JSON.stringify({ idToken }),
+    });
   }
+
+  async function signOutSSR() {
+    const res = await fetch("api/signin", { method: "DELETE" });
+    await signOut(auth);
+  }
+
   const user = userStore(auth);
-  // console.log(user);
+  // console.log($user);
 </script>
 
 {#if $user}
   <h2 class="card-title">Welcome, {$user.displayName}</h2>
   <p class="text-center text-success">You are logged in!</p>
   <div class="flex flex-row gap-5">
-    <button
-      class="btn btn-warning"
-      on:click={() => {
-        signOut(auth);
-      }}>Sign out!</button
-    >
+    <button class="btn btn-warning" on:click={signOutSSR}>Sign out!</button>
     <a href="login/username" class="btn btn-primary">⮕ Next</a>
   </div>
 {:else}
